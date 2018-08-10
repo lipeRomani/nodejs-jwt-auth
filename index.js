f5a924e153fe2cdc5a215ee840e5d5c731578331
Felipe Romani <felipearomani@gmail.com>
2018-08-09 22:26:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+const PORT = process.env.PORT || 3090;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listen port ${PORT}`);
+});
